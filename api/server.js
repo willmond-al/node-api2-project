02ba2a468fcc21612ad50424883bb9b2f6e8fbe5
willmondal <wbmondal@aol.com>
2021-02-09 13:03:59 -0800

    get, and put done but with bugs

--- api/server.js
+++ api/server.js
@@ -1,2 +1,75 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express')
+
+const server = express()
+
+const Posts = require('./posts/posts-model')
+
+server.get('/api/posts', (req, res) => {
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'error retrieving posts'
+        })
+    })
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: `no post with id ${req.params.id}`
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: `error retrieving post with id ${req.params.id}`
+        })
+    })
+})
+
+server.post('/api/posts', (req, res) => {
+    Posts.add(req.body)
+    .then(post => {
+        res.status(201).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'error adding post'
+        })
+    })
+})
+
+server.put('/api/posts/:id', (req, res) => {
+    Posts.update(req.params.id, req.body)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({
+                message: `post id ${req.parama.id} not found`
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'error updating post'
+        })
+    })
+})
+
+
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server')
+const PORT = 3000
+
+server.listen(PORT, ()=>{
+    console.log(`Server running on http://localhost:${PORT}`)
+})
